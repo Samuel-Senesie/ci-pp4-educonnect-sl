c8dace01fa6d09d983bc83c10a4ac0525dc00616
Samuel Senesie <samuelsenesie@gmail.com>
2024-10-18 20:03:35 +0000

    update development environment

--- educonnect/settings.py
+++ educonnect/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-r29qq0su=^+szhj*^q!^%!iyf#@*ys=(3)o&vp+n=2z95ck#9d
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['8000-samuelsenes-cipp4educon-9zhjqt24jnx.ws.codeinstitute-ide.net' ,'.herokuapp.com']
+ALLOWED_HOSTS = ['8000-samuelsenes-cipp4educon-9zhjqt24jnx.ws.codeinstitute-ide.net','.herokuapp.com']
 
 
 # Application definition
