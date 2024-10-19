3908984776f9550ba2efa54b76cfb627869b55d6
Samuel Senesie <samuelsenesie@gmail.com>
2024-10-19 13:22:01 +0000

    deploy postgress database to Heroku

--- educonnect/settings.py
+++ educonnect/settings.py
@@ -25,6 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-r29qq0su=^+szhj*^q!^%!iyf#@*ys=(3)o&vp+n=2z95ck#9d'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
