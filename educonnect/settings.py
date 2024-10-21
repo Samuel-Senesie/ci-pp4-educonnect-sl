343ac95c7169ea13767db6b970fa6299a2e19e25
Samuel Senesie <samuelsenesie@gmail.com>
2024-10-21 20:08:11 +0000

    Add Userprofile model

--- educonnect/settings.py
+++ educonnect/settings.py
@@ -91,8 +91,9 @@ DATABASES = {
 }
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://samuelsenes-cipp4educon-9oc5lbky0f0.ws.codeinstitute-ide.net/",
-    "https://educonnect-sl-5ffcb9da3e93.herokuapp.com/"
+    "https://samuelsenes-cipp4educon-9oc5lbky0f0.ws.codeinstitute-ide.net",
+    "https://8000-samuelsenes-cipp4educon-9oc5lbky0f0.ws.codeinstitute-ide.net",
+    "https://educonnect-sl-5ffcb9da3e93.herokuapp.com",
 ]
 
 
@@ -130,6 +131,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATIC_URL = 'static/'
 
 # Default primary key field type
