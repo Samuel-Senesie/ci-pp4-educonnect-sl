6d6cc8e18d843ced45eb0fcd4385e09c49c90c35
Samuel Senesie <samuelsenesie@gmail.com>
2024-11-18 15:32:17 +0000

    Fix database connection and clean up migrations

--- educonnect/settings.py
+++ educonnect/settings.py
@@ -31,11 +31,11 @@ LOGOUT_REDIRECT_URL = 'home'
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'django-insecure-r29qq0su=^+szhj*^q!^%!iyf#@*ys=(3)o&vp+n=2z95ck#9d'
+#SECRET_KEY = 'ew42%t6wr0s#l7t6tqsn_okn(wz499*_(pkvvz6rsnovdlb%=n'
 SECRET_KEY = os.environ.get("SECRET_KEY", 'fallback-defuault-if-not-set')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-samuelsenes-cipp4educon-tfzf7ezlt6v.ws.codeinstitute-ide.net']
 
@@ -104,13 +104,17 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 #}
 
 DATABASE_URLS = os.environ.get("DATABASE_URL", "")
-if DATABASE_URLS:
-
-    DATABASES = {
-        'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
-    }
-else:
+if not DATABASE_URLS:
     raise ValueError("DATABASE_URL is not set. Please check your environment varaibles or .env file")
+
+DATABASES = {
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+}
+
+#DATABASES = {
+#    'default': dj_database_url.config(default=os.environ.get("DATABASE_URL"))
+#}
+    
 
 CSRF_TRUSTED_ORIGINS = [
     "https://samuelsenes-cipp4educon-9oc5lbky0f0.ws.codeinstitute-ide.net",
